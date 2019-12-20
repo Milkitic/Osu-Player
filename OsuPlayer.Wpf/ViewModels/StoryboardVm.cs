ddef69530c122e0529392186d2eed1f40ccd6191
Milky <milkitic@126.com>
2019-12-20 16:55:02 +0800

    new effect

---
+++ OsuPlayer.Wpf/ViewModels/StoryboardVm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Milky.OsuPlayer.Common.Metadata;
+using OSharp.Beatmap.MetaData;
+
+namespace Milky.OsuPlayer.ViewModels
+{
+    class StoryboardVm
+    {
+        public ObservableCollection<BeatmapDataModel> BeatmapModels { get; set; }
+
+        private static StoryboardVm _default;
+        private static object _defaultLock = new object();
+
+        public static StoryboardVm Default
+        {
+            get
+            {
+                lock (_defaultLock)
+                {
+                    return _default ?? (_default = new StoryboardVm());
+                }
+            }
+        }
+
+        private StoryboardVm()
+        {
+        }
+    }
+}
