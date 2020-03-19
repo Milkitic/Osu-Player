5a070962dc652cfd17438d576af9406c0558a0a7
Milkitic <milkitic@126.com>
2020-03-19 17:28:15 +0800

    fix i18n

--- OsuPlayer.Wpf/Utils/Util.cs
+++ OsuPlayer.Wpf/Utils/Util.cs
@@ -42,7 +42,7 @@ namespace Milky.OsuPlayer.Utils
             string strSize = "";
             long factSize = size;
             if (factSize < 1024)
-                strSize = factSize.ToString("F2") + " B";
+                strSize = $"{factSize:F2} B";
             else if (factSize >= 1024 && factSize < 1048576)
                 strSize = (factSize / 1024f).ToString("F2") + " KB";
             else if (factSize >= 1048576 && factSize < 1073741824)
