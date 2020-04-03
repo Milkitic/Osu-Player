7fd9abb22780520a68c947244685a69dcb349289
Milkitic <milkitic@126.com>
2020-04-03 12:02:01 +0800

    move files

---
+++ OsuPlayer.Data/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OsuPlayer.Data
+{
+    public class Class1
+    {
+    }
+}
