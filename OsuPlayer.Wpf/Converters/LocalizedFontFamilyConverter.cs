e78118db91c541a072b5c6f637b4b497fd9f43a2
Milkitic <Milkitic@126.com>
2020-11-14 19:34:46 +0800

    codefactor fixing

--- OsuPlayer.Wpf/Converters/LocalizedFontFamilyConverter.cs
+++ OsuPlayer.Wpf/Converters/LocalizedFontFamilyConverter.cs
@@ -7,21 +7,6 @@ using System.Windows.Media;
 
 namespace Milky.OsuPlayer.Converters
 {
-    public class RoundedNumberConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var d = System.Convert.ToDouble(value);
-            return Math.Round(d, 3);
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            var d = System.Convert.ToDouble(value);
-            return d;
-        }
-    }
-
     public class LocalizedFontFamilyConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
--- OsuPlayer.Wpf/ViewModels/LyricWindowViewModel.cs
+++ OsuPlayer.Wpf/ViewModels/LyricWindowViewModel.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using Milky.OsuPlayer.Common;
+﻿using Milky.OsuPlayer.Common;
 using Milky.OsuPlayer.Common.Configuration;
 using Milky.OsuPlayer.Media.Audio;
 using Milky.OsuPlayer.Presentation.Interaction;
 using Milky.OsuPlayer.Shared.Dependency;
+using Milky.OsuPlayer.Utils;
 using System.Collections.Generic;
-using System.Globalization;
 using System.Linq;
 using System.Windows;
-using System.Windows.Markup;
 using System.Windows.Media;
 
 namespace Milky.OsuPlayer.ViewModels
@@ -59,8 +57,11 @@ namespace Milky.OsuPlayer.ViewModels
             }
         }
 
-        public ICollection<FontFamily> AllFontFamilies { get; } = new SortedSet<FontFamily>(Fonts.SystemFontFamilies.Concat(new[]
-            {(FontFamily) Application.Current.FindResource("SspRegular")}), new FontFamilyComparer());
+        public ICollection<FontFamily> AllFontFamilies { get; } =
+            new SortedSet<FontFamily>(Fonts.SystemFontFamilies.Concat(new[]
+            {
+                (FontFamily) Application.Current.FindResource("SspRegular")
+            }), new FontFamilyComparer());
 
         public double Hue
         {
@@ -92,55 +93,6 @@ namespace Milky.OsuPlayer.ViewModels
                 if (value.Equals(_lightness)) return;
                 _lightness = value;
                 OnPropertyChanged();
-            }
-        }
-    }
-
-    public class FontFamilyComparer : IComparer<FontFamily>
-    {
-        private string _currentCulture = CultureInfo.CurrentUICulture.Name;
-        public int Compare(FontFamily x, FontFamily y)
-        {
-            if (ReferenceEquals(x, y)) return 0;
-            if (ReferenceEquals(null, y)) return 1;
-            if (ReferenceEquals(null, x)) return -1;
-
-            var xNames = x.FamilyNames;
-            var yNames = y.FamilyNames;
-            if (xNames.ContainsKey(XmlLanguage.GetLanguage(_currentCulture)) &&
-                yNames.ContainsKey(XmlLanguage.GetLanguage(_currentCulture)))
-            {
-                var kvpX = xNames[XmlLanguage.GetLanguage(_currentCulture)];
-                var kvpY = yNames[XmlLanguage.GetLanguage(_currentCulture)];
-
-                return string.Compare(kvpX, kvpY, StringComparison.InvariantCulture);
-            }
-            else if (xNames.ContainsKey(XmlLanguage.GetLanguage(_currentCulture)))
-            {
-                return -1;
-            }
-            else if (yNames.ContainsKey(XmlLanguage.GetLanguage(_currentCulture)))
-            {
-                return 1;
-            }
-            else
-            {
-                var kvpX = xNames.FirstOrDefault(k =>
-                    k.Key.IetfLanguageTag != "en-us");
-                var kvpY = yNames.FirstOrDefault(k =>
-                    k.Key.IetfLanguageTag != "en-us");
-                if (kvpX.Key == null) kvpX = xNames.FirstOrDefault();
-                if (kvpY.Key == null) kvpY = yNames.FirstOrDefault();
-
-                if (kvpX.Key != kvpY.Key)
-                {
-                    if (kvpX.Key.IetfLanguageTag == "en-us") return 1;
-                    if (kvpY.Key.IetfLanguageTag == "en-us") return -1;
-
-                    return string.Compare(kvpX.Key.IetfLanguageTag, kvpY.Key.IetfLanguageTag, StringComparison.Ordinal);
-                }
-
-                return string.Compare(kvpX.Value, kvpY.Value, StringComparison.InvariantCulture);
             }
         }
     }
