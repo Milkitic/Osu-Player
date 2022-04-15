da30d0b28db4b257413f8d6c2b3637653af17167
Milkitic <Milkitic@126.com>
2022-04-15 10:28:56 +0800

    Add OsuPlayer.Data

---
+++ OsuPlayer.Data/BeatmapMark.cs
@@ -0,0 +1,37 @@
+﻿using Coosu.Database.DataTypes;
+
+namespace OsuPlayer.Data;
+
+public class BeatmapMark
+{
+    public bool IsAutoManaged { get; set; }
+    public string RelativePath { get; set; } = null!;
+    public BeatmapCachedInfo CachedInfo { get; set; } = null!;
+    public int CachedInfoId { get; set; }
+}
+
+public class BeatmapCachedInfo
+{
+    public string Artist { get; set; } = null!;
+    public string ArtistUnicode { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string TitleUnicode { get; set; } = null!;
+    public string Creator { get; set; } = null!;
+    public string Version { get; set; } = null!;
+    public string BeatmapFileName { get; set; } = null!;
+    public DateTime LastModified { get; set; }
+    public double DefaultStarRatingStd { get; set; }
+    public double DefaultStarRatingTaiko { get; set; }
+    public double DefaultStarRatingCtB { get; set; }
+    public double DefaultStarRatingMania { get; set; }
+    public TimeSpan DrainTime { get; set; }
+    public TimeSpan TotalTime { get; set; }
+    public TimeSpan AudioPreviewTime { get; set; }
+    public int BeatmapId { get; set; }
+    public int BeatmapSetId { get; set; }
+    public DbGameMode GameMode { get; set; }
+    public string Source { get; set; } = null!;
+    public string Tags { get; set; } = null!;
+    public string FolderName { get; set; } = null!;
+    public string AudioFileName { get; set; } = null!;
+}
---
+++ OsuPlayer.Data/OsuDbReaderExtensions.cs
@@ -0,0 +1,74 @@
+﻿using Coosu.Database;
+using Coosu.Database.DataTypes;
+
+namespace OsuPlayer.Data;
+
+public static class OsuDbReaderExtensions
+{
+    public static IEnumerable<BeatmapCachedInfo> EnumerateDbModels(this OsuDbReader reader)
+    {
+        BeatmapCachedInfo? beatmap = null;
+
+        while (reader.Read())
+        {
+            if (reader.NodeType == NodeType.ObjectStart)
+            {
+                beatmap = new BeatmapCachedInfo();
+                continue;
+            }
+
+            if (reader.NodeType == NodeType.ObjectEnd && beatmap != null)
+            {
+                yield return beatmap;
+                beatmap = null;
+            }
+
+            if (reader.NodeType == NodeType.ArrayEnd && reader.NodeId == 7) yield break;
+            if (beatmap == null) continue;
+            if (reader.NodeType is not (NodeType.ArrayStart or NodeType.KeyValue)) continue;
+            FillProperty(reader, reader.NodeId, beatmap);
+        }
+    }
+
+    private static void FillProperty(OsuDbReader reader, int nodeId, BeatmapCachedInfo beatmapInfo)
+    {
+        if (nodeId == 9) beatmapInfo.Artist = reader.GetString();
+        else if (nodeId == 10) beatmapInfo.ArtistUnicode = reader.GetString();
+        else if (nodeId == 11) beatmapInfo.Title = reader.GetString();
+        else if (nodeId == 12) beatmapInfo.TitleUnicode = reader.GetString();
+        else if (nodeId == 13) beatmapInfo.Creator = reader.GetString();
+        else if (nodeId == 14) beatmapInfo.Version = reader.GetString();
+        else if (nodeId == 15) beatmapInfo.AudioFileName = reader.GetString();
+        else if (nodeId == 17) beatmapInfo.BeatmapFileName = reader.GetString();
+        else if (nodeId == 22) beatmapInfo.LastModified = reader.GetDateTime();
+        else if (nodeId == 29) FillStarRating(beatmapInfo, reader, DbGameMode.Circle);
+        else if (nodeId == 32) FillStarRating(beatmapInfo, reader, DbGameMode.Taiko);
+        else if (nodeId == 35) FillStarRating(beatmapInfo, reader, DbGameMode.Catch);
+        else if (nodeId == 38) FillStarRating(beatmapInfo, reader, DbGameMode.Mania);
+        else if (nodeId == 40) beatmapInfo.DrainTime = TimeSpan.FromSeconds(reader.GetInt32());
+        else if (nodeId == 41) beatmapInfo.TotalTime = TimeSpan.FromMilliseconds(reader.GetInt32());
+        else if (nodeId == 42) beatmapInfo.AudioPreviewTime = TimeSpan.FromMilliseconds(reader.GetInt32());
+        else if (nodeId == 46) beatmapInfo.BeatmapId = reader.GetInt32();
+        else if (nodeId == 47) beatmapInfo.BeatmapSetId = reader.GetInt32();
+        else if (nodeId == 55) beatmapInfo.GameMode = (DbGameMode)reader.GetByte();
+        else if (nodeId == 56) beatmapInfo.Source = reader.GetString();
+        else if (nodeId == 57) beatmapInfo.Tags = reader.GetString();
+        else if (nodeId == 63) beatmapInfo.FolderName = reader.GetString();
+    }
+
+    private static void FillStarRating(BeatmapCachedInfo beatmapInfo, OsuDbReader osuDbReader, DbGameMode index)
+    {
+        while (osuDbReader.Read())
+        {
+            if (osuDbReader.NodeType == NodeType.ArrayEnd) break;
+            var data = osuDbReader.GetIntDoublePair();
+            var mods = (Mods)data.IntValue;
+            if (mods != Mods.None) continue;
+
+            if (index == DbGameMode.Circle) beatmapInfo.DefaultStarRatingStd = data.DoubleValue;
+            else if (index == DbGameMode.Taiko) beatmapInfo.DefaultStarRatingTaiko = data.DoubleValue;
+            else if (index == DbGameMode.Catch) beatmapInfo.DefaultStarRatingCtB = data.DoubleValue;
+            else if (index == DbGameMode.Mania) beatmapInfo.DefaultStarRatingMania = data.DoubleValue;
+        }
+    }
+}
