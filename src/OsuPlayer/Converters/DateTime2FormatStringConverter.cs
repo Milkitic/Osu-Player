bf6dbc51abda3cfb690c7de7b23fe7462a06b488
Milkitic <Milkitic@126.com>
2022-08-26 17:44:52 +0800

    ExportPage Optimize

---
+++ src/OsuPlayer/Converters/DateTime2FormatStringConverter.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace Milki.OsuPlayer.Converters;
+
+public class DateTime2FormatStringConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is DateTime dt)
+        {
+            try
+            {
+                if (dt == DateTime.MinValue) return "unknown";
+                string format = parameter is string s ? s : null;
+                return string.IsNullOrEmpty(format)
+                    ? dt.ToString("g")
+                    : dt.ToString(format);
+            }
+            catch (Exception e)
+            {
+                return dt.ToString(CultureInfo.CurrentCulture);
+            }
+        }
+
+        return "unknown";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
