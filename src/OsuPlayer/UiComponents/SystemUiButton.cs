f0b2731fb406d4bc21c805537331fccb33c8791d
Milkitic <Milkitic@126.com>
2022-09-08 13:27:07 +0800

    Update templates

--- src/OsuPlayer/UiComponents/SystemUiButton.cs
+++ src/OsuPlayer/UiComponents/SystemUiButton.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows;
-using Milki.OsuPlayer.UiComponents.ButtonComponent;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace Milki.OsuPlayer.UiComponents;
 
@@ -7,12 +10,12 @@ public class SystemUiButton : UiButton
 {
     public SystemUiButton()
     {
-        this.Loaded += SystemButton_Loaded;
+        Loaded += Button_OnLoaded;
     }
 
     protected Window HostWindow { get; private set; }
 
-    private void SystemButton_Loaded(object sender, RoutedEventArgs e)
+    private void Button_OnLoaded(object sender, RoutedEventArgs e)
     {
         if (HostWindow != null)
         {
@@ -25,9 +28,24 @@ public class SystemUiButton : UiButton
 
 public class CloseUiButton : SystemUiButton
 {
+    private const string ResourceCloseTemplString =
+        "    <ControlTemplate x:Key=\"CloseTempl\">\r\n        <Canvas Width=\"1024\" Height=\"1024\">\r\n            <Path Fill=\"{TemplateBinding Foreground}\">\r\n                <Path.Data>\r\n                    <PathGeometry Figures=\"M886.592 841.344L557.248 512l329.36-329.36a32 32 0 1 0-45.264-45.232L512 466.752 182.656 137.408a32 32 0 1 0-45.264 45.232L466.752 512 137.408 841.344a32 32 0 1 0 45.232 45.264L512 557.248l329.36 329.36a32 32 0 1 0 45.232-45.264z\" FillRule=\"NonZero\" />\r\n                </Path.Data>\r\n            </Path>\r\n        </Canvas>\r\n    </ControlTemplate>";
+
+    private static readonly ControlTemplate CloseTempl;
+    private static readonly Brush StaticMouseOverBackground;
+
+    static CloseUiButton()
+    {
+        CloseTempl = System.Windows.Markup.XamlReader.Parse(ResourceCloseTemplString) as ControlTemplate;
+        StaticMouseOverBackground = (Brush)new BrushConverter().ConvertFrom("#F0F72F2F");
+    }
+
     public CloseUiButton()
     {
-        this.Click += OnClick;
+        IconSize = 16;
+        IconTemplate = CloseTempl;
+        MouseOverBackground = StaticMouseOverBackground;
+        Click += OnClick;
     }
 
     private void OnClick(object sender, RoutedEventArgs args)
@@ -38,9 +56,21 @@ public class CloseUiButton : SystemUiButton
 
 public class MinUiButton : SystemUiButton
 {
+    private const string ResourceMinimizeTemplString =
+        "    <ControlTemplate x:Key=\"MinimizeTempl\">\r\n        <Canvas Width=\"1024\" Height=\"1024\">\r\n            <Path Fill=\"{TemplateBinding Foreground}\">\r\n                <Path.Data>\r\n                    <PathGeometry Figures=\"M797.291117 486.21473 224.18848 486.21473c-14.078647 0-25.469068 11.342326-25.469068 25.472138 0 14.028505 11.390421 25.471115 25.469068 25.471115l573.101613 0c14.07967 0 25.470091-11.441587 25.470091-25.471115C822.760185 497.557056 811.370787 486.21473 797.291117 486.21473z\" FillRule=\"NonZero\" />\r\n                </Path.Data>\r\n            </Path>\r\n        </Canvas>\r\n    </ControlTemplate>";
+
+    private static readonly ControlTemplate MinimizeTempl;
+
+    static MinUiButton()
+    {
+        MinimizeTempl = System.Windows.Markup.XamlReader.Parse(ResourceMinimizeTemplString) as ControlTemplate;
+    }
+
     public MinUiButton()
     {
-        this.Click += OnClick;
+        IconMargin = new Thickness(0, 8, 0, 0);
+        IconTemplate = MinimizeTempl;
+        Click += OnClick;
     }
 
     private void OnClick(object sender, RoutedEventArgs args)
@@ -52,42 +82,55 @@ public class MinUiButton : SystemUiButton
     }
 }
 
-public class MaxUiButton : SystemUiButton
+public class MaxUiButton : SystemUiButton, INotifyPropertyChanged
 {
-    public static readonly DependencyProperty IsWindowMaxProperty = DependencyProperty.Register(nameof(IsWindowMax), typeof(bool), typeof(SystemButton), new PropertyMetadata(false));
+    private const string ResourceRecoverTemplString = "    <ControlTemplate x:Key=\"RecoverTempl\">\r\n        <Canvas Width=\"1024\" Height=\"1024\">\r\n            <Path Fill=\"{TemplateBinding Foreground}\">\r\n                <Path.Data>\r\n                    <PathGeometry Figures=\"M512 1255.489906\" FillRule=\"NonZero\" />\r\n                </Path.Data>\r\n            </Path>\r\n            <Path Fill=\"{TemplateBinding Foreground}\">\r\n                <Path.Data>\r\n                    <PathGeometry Figures=\"M865.682191 310.085948l-554.675195 0c-14.634419 0-26.403358 11.973616-26.403358 26.710374L284.603638 423.681791l-92.309414 0c-14.634419 0-26.403358 11.973616-26.403358 26.710374l0 349.998001c0 14.634419 11.768939 26.505697 26.403358 26.505697l554.675195 0c14.634419 0 26.710374-11.871277 26.710374-26.505697L773.679792 713.30002l92.002399 0c14.634419 0 26.710374-11.871277 26.710374-26.505697l0-349.998001C892.392564 322.059564 880.31661 310.085948 865.682191 310.085948zM728.65081 781.86688 210.817509 781.86688 210.817509 468.710774l517.8333 0L728.65081 781.86688zM847.363582 668.271037l-73.68379 0L773.679792 450.392165c0-14.634419-12.075954-26.710374-26.710374-26.710374L329.530282 423.681791l0-68.56686 517.8333 0L847.363582 668.271037z\" FillRule=\"NonZero\" />\r\n                </Path.Data>\r\n            </Path>\r\n        </Canvas>\r\n    </ControlTemplate>";
+    private const string ResourceMaximizeTemplString = "    <ControlTemplate x:Key=\"MaximizeTempl\">\r\n        <Canvas Width=\"1024\" Height=\"1024\">\r\n            <Path Fill=\"{TemplateBinding Foreground}\">\r\n                <Path.Data>\r\n                    <PathGeometry Figures=\"M204.8 256a51.2 51.2 0 0 0-51.2 51.2v409.6a51.2 51.2 0 0 0 51.2 51.2h614.4a51.2 51.2 0 0 0 51.2-51.2V307.2a51.2 51.2 0 0 0-51.2-51.2H204.8z m0-51.2h614.4a102.4 102.4 0 0 1 102.4 102.4v409.6a102.4 102.4 0 0 1-102.4 102.4H204.8a102.4 102.4 0 0 1-102.4-102.4V307.2a102.4 102.4 0 0 1 102.4-102.4z\" FillRule=\"NonZero\" />\r\n                </Path.Data>\r\n            </Path>\r\n        </Canvas>\r\n    </ControlTemplate>";
+
+    public static readonly ControlTemplate MaximizeTempl;
+    public static readonly ControlTemplate RecoverTempl;
+
     private bool _isSigned;
+    private bool _isWindowMax;
+
+    static MaxUiButton()
+    {
+        RecoverTempl = System.Windows.Markup.XamlReader.Parse(ResourceRecoverTemplString) as ControlTemplate;
+        MaximizeTempl = System.Windows.Markup.XamlReader.Parse(ResourceMaximizeTemplString) as ControlTemplate;
+    }
 
     public MaxUiButton()
     {
-        this.Click += OnClick;
+        IconTemplate = MaximizeTempl;
+        Click += OnClick;
     }
 
     public bool IsWindowMax
     {
-        get => (bool)GetValue(IsWindowMaxProperty);
-        set => SetValue(IsWindowMaxProperty, value);
+        get => _isWindowMax;
+        set => SetField(ref _isWindowMax, value);
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetField<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value)) return false;
+        field = value;
+        OnPropertyChanged(propertyName);
+        return true;
     }
 
     private void OnClick(object sender, RoutedEventArgs args)
     {
         if (HostWindow == null) return;
-        
-        if (!_isSigned)
-        {
-            HostWindow.StateChanged += delegate
-            {
-                if (HostWindow.WindowState == WindowState.Normal)
-                {
-                    IsWindowMax = false;
-                }
-                else if (HostWindow.WindowState == WindowState.Maximized)
-                {
-                    IsWindowMax = true;
-                }
-            };
 
-            _isSigned = true;
-        }
+        SignUpEvent();
 
         if (HostWindow.WindowState == WindowState.Normal)
         {
@@ -96,6 +139,32 @@ public class MaxUiButton : SystemUiButton
         else if (HostWindow.WindowState == WindowState.Maximized)
         {
             HostWindow.WindowState = WindowState.Normal;
+        }
+    }
+
+    private void SignUpEvent()
+    {
+        if (_isSigned) return;
+        HostWindow.StateChanged += (_, _) => RecoverState();
+
+        _isSigned = true;
+    }
+
+    private void RecoverState()
+    {
+        if (HostWindow.WindowState == WindowState.Normal)
+        {
+            IsWindowMax = false;
+            IconTemplate = MaximizeTempl;
+            IconMargin = new Thickness(0);
+            IconSize = 20;
+        }
+        else if (HostWindow.WindowState == WindowState.Maximized)
+        {
+            IsWindowMax = true;
+            IconTemplate = RecoverTempl;
+            IconMargin = new Thickness(0, 0, 0, 3);
+            IconSize = 23;
         }
     }
 }
